bfc55d6639ff19bd9d0e37d329a6270fc37e065c
fitrianinasir <fitrianinasir8@gmail.com>
2020-10-18 14:50:58 +0800

    example

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+function App() {
+  const position = [-5.1740328, 119.4427]
+  const zoom = 13 
+
+  return (
+    <div className="App">
+       <Map center={position} zoom={zoom} style={{height : '400px'}}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
+          </Popup>
+        </Marker>
+      </Map>
+    </div>
+  );
+}
+
+export default App;
